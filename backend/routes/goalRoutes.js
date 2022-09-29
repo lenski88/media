77c863eaa1c5ee41c866beef54ceca9254b02f26
Alexander Lenski <lenski88@icloud.com>
2022-09-30 00:01:31 +0300

    add endpoints

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const { getGoals, 
+        postGoals, 
+        putGoal, 
+        deleteGoal
+    } = require('../controllers/goalController');
+
+router.route('/').get(getGoals).post(postGoals);
+router.route('/:id').put(putGoal).delete(deleteGoal);
+
+module.exports = router;
